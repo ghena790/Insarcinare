59b8b06538bba52a2e1f4d55cbff3f3004b0712d
ghena790 <43550447+ghena790@users.noreply.github.com>
2020-09-25 12:41:00 +0300

    Add files via upload

---
+++ aside.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <style>
+        aside {
+            margin-top: 10px;
+            width: 20%;
+            padding-right: 15px;
+            margin-right: 15px;
+            float: left;
+            font-style: italic;
+            background-color: lightskyblue;
+        }
+    </style>
+</head>
+<body>
+<aside >
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+</aside>
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="header">
+    <a href="#default" class="logo">Ghena</a>
+
+    </div>
+</div>
+<style>
+
+    .header {
+        overflow: hidden;
+        background-color: Blue;
+        padding: 20px 10px;
+    }
+
+    .header a {
+        float: Right;
+        color: black;
+        padding: 12px;
+        text-decoration: none;
+        font-size: 38px;
+        line-height: 25px;
+        border-radius: 4px;
+    }
+
+
+</style>
+
+</body>
+</html>
